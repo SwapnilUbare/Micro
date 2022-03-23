4d187f27cc2943ccfa1a2d4f916a97d24ab1ec26
anisabargir <anisabargir24@gmail.com>
2022-03-23 18:08:00 +0530

    add hii

--- src/main/java/com/codedcode/microservice/EurekaServer/EurekaServerApplication.java
+++ src/main/java/com/codedcode/microservice/EurekaServer/EurekaServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 //hiii
+//hello
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
